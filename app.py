7e7a67b33561dcb4bb1c6fa6a8ac4a5bbc3e03f1
mart <martindore21@gmail.com>
2024-11-13 16:59:51 +0100

    add errors handling

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.config['SECRET_KEY'] = 'your_secret_key'  # Clé secrète pour CSRF
 class MyForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     birthday = DateField('Birthday', format='%Y-%m-%d', validators=[DataRequired()])
+    submit = SubmitField('Submit form')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,12 +17,29 @@ def home():
     form = MyForm()
     if form.validate_on_submit():
         # Traiter les données du formulaire ici
-        return redirect(url_for('success'))
-    return render_template('index.html')
+        return render_template('name.html', name = form.name.data)
+    return render_template('index.html',
+                           form=form)
 
 @app.route('/success')
 def success():
     return "Form successfully submitted!"
 
+# ERRORS
+
+# Invalid URL / Pagename
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Servor Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
